05e5b6b30662aa31f71d020128fdef2f22d03565
Bilguun Ochirbat <bilguun0203@gmail.com>
2018-11-13 17:08:04 +0800

    helper.py

---
+++ helper.py
@@ -0,0 +1,21 @@
+import pickle
+
+
+# Сургалтын явцыг харуулах
+def print_progress(count, total, bar_len=60):
+    percent = round(100.0 * count / float(total), 1)
+    filled_len = int(round(bar_len * count / float(total)))
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    print('\r[{0}] {1}% {2}/{3}\r'.format(bar, percent, count, total), flush=True, end='\r')
+
+
+# Neural Network файлаас унших
+def nn_load(filename):
+    with open(filename, 'rb') as input:
+        return pickle.load(input)
+
+
+# Neural Network файлд хадгалах
+def nn_save(filename, nn):
+    with open(filename, 'wb') as output:
+        pickle.dump(nn, output, pickle.HIGHEST_PROTOCOL)
